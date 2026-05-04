e042bd43c68f95e81d931d9a25b35f03aac08f3b
Sarwar Hussain <sarwarahmed693@gmail.com>
2026-05-04 22:29:12 +0600

    deployment2

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,5 @@
 export const data = async () => {
-  const res = await fetch("http://localhost:3000/data.json", {
+  const res = await fetch("https://qurbani-khaki.vercel.app/data.json", {
     cache: "no-store",
   });
   const animalData = await res.json();

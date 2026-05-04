ad27782a747b0021572b524be41c4322845a5729
Sarwar Hussain <sarwarahmed693@gmail.com>
2026-05-04 23:41:59 +0600

    deployment7

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,7 +1,5 @@
 export const data = async () => {
-  const res = await fetch("https://qurbani-three.vercel.app/data.json", {
-    cache: "no-store",
-  });
+  const res = await fetch("https://qurbani-three.vercel.app/data.json");
   const animalData = await res.json();
   return animalData;
 };
